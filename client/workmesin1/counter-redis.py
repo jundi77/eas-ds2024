f88541f911302ffd934f4300fde66cf9e163f19b
jundi77 <jundi@jhr.my.id>
2024-12-18 21:35:03 +0700

    fix client stop if fail and add network capture for case study

--- client/workmesin1/counter-redis.py
+++ client/workmesin1/counter-redis.py
@@ -37,35 +37,72 @@ def find_key_owner(key, cluster):
                         node_host = node_id.split(':')[0]
                         if node_host == '':
                             continue # host may be dead or something, the host value just gone lol
-                        node_host = socket.gethostbyaddr(node_host)[0]
+                        name = socket.gethostbyaddr(node_host)[0]
+                        if name.find('eas-ds2024-') != -1:
+                            node_host = socket.gethostbyaddr(node_host)[0].split('.')[0]
+
                         print(f'Key {key} with keyslot {keyslot} is stored on node {node_host}')
                         return
                 elif keyslot == slot_range:
                     node_host = node_id.split(':')[0]
                     if node_host == '':
                         continue # host may be dead or something, the host value just gone lol
-                    node_host = socket.gethostbyaddr(node_host)[0]
+                    name = socket.gethostbyaddr(node_host)[0]
+                    if name.find('eas-ds2024-') != -1:
+                        node_host = socket.gethostbyaddr(node_host)[0].split('.')[0]
                     print(f'Key {key} with keyslot {keyslot} is stored on node {node_host}')
                     return
 
 def print_simplified_nodes(cluster):
-    for node_id, node_info in cluster.cluster_nodes().items():
+    node_host_id = {}
+    nodes = cluster.cluster_nodes()
+    for node_id, node_info in nodes.items():
         node_host = node_id.split(':')[0]
         if node_host == '':
             continue # host may be dead or something, the host value just gone lol
-        node_host = socket.gethostbyaddr(node_host)[0]
-        print(f'{node_host}: {node_info["flags"]}')
+        name = socket.gethostbyaddr(node_host)[0]
+        if name.find('eas-ds2024-') != -1:
+            node_host = socket.gethostbyaddr(node_host)[0].split('.')[0]
+        node_host_id[node_info['node_id']] = node_host
+
+    for node_id, node_info in nodes.items():
+        node_host = node_id.split(':')[0]
+        if node_host == '':
+            continue # host may be dead or something, the host value just gone lol
+        name = socket.gethostbyaddr(node_host)[0]
+        if name.find('eas-ds2024-') != -1:
+            node_host = socket.gethostbyaddr(node_host)[0].split('.')[0]
+        if 'slave' in node_info['flags']:
+            print(f'{node_host}: {node_info["flags"]}, master: {node_host_id[node_info["master_id"]]}')
+        else:
+            print(f'{node_host}: {node_info["flags"]}')
 
 print_simplified_nodes(cluster)
 counter_key = "global_counter"
 find_key_owner(counter_key, cluster)
 
 while True:
-    print('-' * 15)
-    new_value = cluster.incr(counter_key)
-    print(f"INCR: '{counter_key}' = {new_value}")
-    nodes = [node.host for node in cluster.get_nodes()]
-    print_simplified_nodes(cluster)
-    find_key_owner(counter_key, cluster)
+    try:
+        print('-' * 15)
+        new_value = cluster.incr(counter_key)
+        print(f"INCR: '{counter_key}' = {new_value}")
+        nodes = [node.host for node in cluster.get_nodes()]
+        print_simplified_nodes(cluster)
+        find_key_owner(counter_key, cluster)
+    except Exception:
+        while True:
+            try:
+                host = nodes[random.randint(0, len(nodes) - 1)]
+                print(f'Trying first host {host}...')
+                cluster = redis.RedisCluster(
+                    host=host,
+                    port= 6379,
+                    decode_responses=True
+                )
+                nodes = [node.host for node in cluster.get_nodes()]
+            except Exception:
+                print(f'Exception with host {host}.')
+                continue
+            break
 
     time.sleep(random.uniform(0.75, 1.3))
